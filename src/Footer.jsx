5a094546b747ff40be7c32d398fd24de5e301ae5
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2023-08-02 16:30:08 +0700

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,6 +2,12 @@ export default function Footer() {
     return (
         <footer className="fixed bottom-0 underline w-full bg-blue-300">
         <div className="flex px-20 py-3 text justify-between items-center">
+          <a href="/">Home</a>
+          <a href="/example">Example</a>
+          <a href="/impure-function">Impure Function</a>
+          <a href="/pure-function">Pure Function</a>
+          <a href="/statejs">StateJS</a>
+          <a href="/statereactjs">StateReactJS</a>
           <a href="/multiplestate">Multiple State</a>
           <a href="/queuePrevState">Queue Prev State</a>
           <a href="/refvalue">Ref Value</a>
