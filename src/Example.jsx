f35b45c87b1654682985fd689105ceba0ddcf6a6
Cullen179 <itslamemail@gmail.com>
2023-08-03 08:33:58 +0700

    Update Example.jsx

--- src/Example.jsx
+++ src/Example.jsx
@@ -24,12 +24,12 @@ export default function App() {
   const items = ['Item 1', 'Item 2', 'Item 3'];  
 
   return ( 
-    <div> 
+    <> 
       {/* Using the ExampleComponent with props */} 
       <ExampleComponent title="Example Component" showItems items={items}> 
         {/* Adding additional content as children */} 
         <p>This is a child element.</p> 
       </ExampleComponent> 
-    </div> 
+    </> 
   ); 
 } 
